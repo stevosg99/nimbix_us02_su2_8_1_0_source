598f0ceeb925e8d1c37dc0e455fcead0b0e67267
stevosg99 <96193330+stevosg99@users.noreply.github.com>
2024-10-07 14:42:09 -0500

    Update compile_SU2.sh

--- compile_SU2.sh
+++ compile_SU2.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
 # Ensure the current working directory
-initdir=/tmp/SU2/
+initdir=/data/SU2/
 cd $initdir
 git clone --branch v8.1.0 https://github.com/su2code/SU2.git
-wkdir=/tmp/SU2/SU2
+wkdir=/data/SU2/SU2
 cd $wkdir
 
 # Set the initial environmental variables
-export MPICC=/tmp/JARVICE/openmpi/bin/mpicc
-export MPICXX=/tmp/JARVICE/openmpi/bin/mpicxx
+export MPICC=/data/JARVICE/openmpi/bin/mpicc
+export MPICXX=/data/JARVICE/openmpi/bin/mpicxx
 export CC=$MPICC
 export CXX=$MPICXX
 export CXXFLAGS="-O2 -funroll-loops -march=native -mtune=native"
@@ -34,9 +34,9 @@ while [ "$build_counter" -le 3 ]; do
 	./meson.py build $flags --prefix=$initdir/install | tee -a build_log.txt
 
 	# Set environmental variables from meson build
-	export SU2_DATA=/tmp/SU2/
-	export SU2_HOME=/tmp/SU2/SU2
-	export SU2_RUN=/tmp/SU2/SU2/bin
+	export SU2_DATA=/data/SU2/
+	export SU2_HOME=/data/SU2/SU2
+	export SU2_RUN=/data/SU2/SU2/bin
 	export PATH=$PATH:$SU2_RUN
 	export PYTHONPATH=$PYTHONPATH:$SU2_RUN
 	# Set environmental variable to allow multi-node use
@@ -64,4 +64,4 @@ elif [ $build_counter -eq 10 ]; then
 fi
 
 # Record a ready status
-touch /tmp/node_ready_status.txt
+touch /data/node_ready_status.txt
